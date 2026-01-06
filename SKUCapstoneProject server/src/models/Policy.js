66ec11161c5901b3c1828417ec2a30248df96994
harubbonbing <kloaa394@gmail.com>
2026-01-06 21:54:11 +0900

    sku
    sku

--- SKUCapstoneProject server/src/models/Policy.js
+++ SKUCapstoneProject server/src/models/Policy.js
@@ -6,6 +6,6 @@ const policySchema = new mongoose.Schema({
     소관부처명: String,
     소관조직명: String,
     서비스URL: String
-}, { collection: 'policy' }); // 컬렉션 이름이 'policy'임을 명시
+}, { collection: 'firststep' }); // 컬렉션 이름이 'policy'임을 명시
 
-module.exports = mongoose.model('policy', policySchema, policy);
+module.exports = mongoose.model('Policy', policySchema, 'policy');
