47128dd2bfedcaad251041363b3768dbd222386a
harubbonbing <kloaa394@gmail.com>
2026-01-07 19:31:07 +0900

    s
    s

--- SKUCapstoneProject server/src/routes/policyRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const policyController = require('../controllers/policyController');
-
-// GET /api/policies 경로로 요청이 오면 컨트롤러 실행
-router.get('/', policyController.getAllPolicies);
-
-module.exports = router;
